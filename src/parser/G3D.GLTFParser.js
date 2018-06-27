aa44913f24232da1a4dee05227695df96d0625e3
xieguanglei <xieguanglei@hotmail.com>
2018-06-27 09:49:16 +0800

    prepare gltf model

--- src/parser/G3D.GLTFParser.js
+++ src/parser/G3D.GLTFParser.js
@@ -1,14 +1,26 @@
-
-
+import MeshBuilder from "../mesh/G3D.MeshBuilder";
 
 const parse = (gltf, scene) => {
-
     console.log(gltf);
 
+    const meshes = gltf.meshes.map(item => {
+
+        const mesh = new Mesh(scene);
+
+        const { primitives, name } = item;
+
+        // only support one primitive
+        const { attributes, indices, mode } = primitives[0];
+
+
+
+
+    });
+
+
+
+
 }
-
-
-
 
 
 export default { parse };
